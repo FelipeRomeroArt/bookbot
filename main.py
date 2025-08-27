9785bb4fc7da1e561447e2db2a4051d3af6dadfc
@FelipeRomeroArt <felipegarciarom@gmail.com>
2025-08-27 21:12:31 +0200

    antes de refactor

---
+++ main.py
@@ -0,0 +1,15 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    print(f"{num_words} words found in the document")
+
+main()
