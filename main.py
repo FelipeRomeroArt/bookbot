578f81b307e07e9190853bcb8bf322d8a3d5d10a
@FelipeRomeroArt <felipegarciarom@gmail.com>
2025-08-28 01:06:01 +0200

    sorteado con exito

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from stats import word_count, char_count
+from stats import word_count, char_count, sort_char_dict
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
+    print("============ BOOKBOT ============")
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    print(f"Analyzing book found at {book_path}...")
     num_words = word_count(text)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
     char_dir = char_count(text)
-    print(f"{num_words} words found in the document")
-    print(char_dir)
+    sort_dict = sort_char_dict(char_dir)
+    print("--------- Character Count -------")
+    j = 0
+    for i in sort_dict:
+        if sort_dict[j]["char"].isalpha():
+            print(f"{sort_dict[j]["char"]}: {sort_dict[j]["num"]}")
+        j += 1
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,19 @@ def char_count(text):
         else:
             char_dir[lowchar] = 1
     return char_dir
+
+def sort_on(items):
+    return items["num"]
+
+def sort_char_dict(char_dir):
+    list_of_dicts = []
+    for char in char_dir:
+        num = char_dir[char]
+        tmp_dict = {
+                "char": char,
+                "num": num
+                }
+        list_of_dicts.append(tmp_dict)
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
