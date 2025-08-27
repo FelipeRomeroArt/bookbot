b84fbfbf974bce64651a7e5748ecef254587309b
@FelipeRomeroArt <felipegarciarom@gmail.com>
2025-08-27 23:06:50 +0200

    char stats

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, char_count
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,6 +8,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
+    char_dir = char_count(text)
     print(f"{num_words} words found in the document")
+    print(char_dir)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def word_count(text):
     words = text.split()
     return len(words)
 
+def char_count(text):
+    char_dir = {}
+    for char in text:
+        lowchar = char.lower()
+        if lowchar in char_dir:
+            char_dir[lowchar] += 1
+        else:
+            char_dir[lowchar] = 1
+    return char_dir
