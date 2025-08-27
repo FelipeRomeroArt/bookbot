11e3069dacdac67ff2a3fd09159d2421e65310d7
@FelipeRomeroArt <felipegarciarom@gmail.com>
2025-08-28 01:39:35 +0200

    argvs gestionados

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+import sys
 from stats import word_count, char_count, sort_char_dict
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def args(argv):
+    if len(argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    elif len(argv) == 2:
+        path = argv[1]
+    return path
+
 def main():
+    book_path = args(sys.argv)
     print("============ BOOKBOT ============")
-    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     print(f"Analyzing book found at {book_path}...")
     num_words = word_count(text)
